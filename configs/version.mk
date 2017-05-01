8cf434cbca5e56cf305633c90bdba9ed1e41f1a2
viperusgit <viperusgit@gmail.com>
2017-05-01 00:30:31 -0400

    Vanilla Cheesecake

--- configs/version.mk
+++ configs/version.mk
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VANILLA_BUILDNAME :=C
+VANILLA_BUILDNAME :=Cheesecake
 VANILLA_BUILDNUMBER :=3.0
 ifeq ($(PUBLIC),true)
  VANILLA_BUILDTAG :=Public

50ac901d8936a54599fa6c321eb8a62ba360e24d
viperusgit <viperusgit@gmail.com>
2017-06-03 23:37:32 -0400

    Play time is over... sadly.
    Its okay
    We had a very succesful troll of (not so) android open source
    paranoid android
    now a new version is beginning to be born
    Welcome to Vanilla Deviant
    This is our time to Juneify vanilla.

--- configs/version.mk
+++ configs/version.mk
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VANILLA_BUILDNAME :=PRESS-RELEASE
+VANILLA_BUILDNAME :=Deviant
 VANILLA_BUILDNUMBER :=4.0
 ifeq ($(PUBLIC),true)
  VANILLA_BUILDTAG :=Public

3af02a9ca519f8ee5305840ee2247ee095859926
Skye <viperusgit@gmail.com>
2017-07-05 15:09:28 -0400

    Vanilla Extract is coming (July update)

--- configs/version.mk
+++ configs/version.mk
@@ -13,8 +13,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-VANILLA_BUILDNAME :=Deviant
-VANILLA_BUILDNUMBER :=4.3
+VANILLA_BUILDNAME :=Extract
+VANILLA_BUILDNUMBER :=5.0
 ifeq ($(PUBLIC),true)
  VANILLA_BUILDTAG :=Public
 else
